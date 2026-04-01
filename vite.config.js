a5369c1c3e7151a58bd44ea7076c0e3cd5c82c56
PCDC1975 <paulocdchaves@gmail.com>
2026-03-31 23:18:52 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/meu-projeto/',
+  base: '/meu-projeto/',
   plugins: [
     vue(),
     vueDevTools(),
